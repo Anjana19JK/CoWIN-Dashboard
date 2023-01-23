fab0ab236060b1f99db86706462396915f2bc7c7
Anjana19JK <anjanajoseph199@gmail.com>
2023-01-23 18:36:21 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,34 +1,33 @@
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 const VaccinationByAge = props => {
   const {graphDetails} = props
   const {count, age} = graphDetails
 
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <PieChart>
-        <Pie
-          cx="70%"
-          cy="40%"
-          data={age}
-          startAngle={0}
-          endAngle={360}
-          outerRadius="100%"
-          dataKey={count}
-        >
-          <Cell name="18-44" fill="#fecba6" />
-          <Cell name="44-60" fill="#b3d23f" />
-          <Cell name="Above 60" fill="#a44c9e" />
-        </Pie>
-        <Legend
-          iconType="circle"
-          layout="vertical"
-          verticalAlign="middle"
-          align="right"
-        />
-      </PieChart>
-    </ResponsiveContainer>
+    <PieChart width={1000} height={300}>
+      <Pie
+        cx="70%"
+        cy="40%"
+        data={age}
+        startAngle={0}
+        endAngle={360}
+        outerRadius="100%"
+        dataKey={count}
+      >
+        <Cell name="18-44" fill="#fecba6" />
+        <Cell name="44-60" fill="#b3d23f" />
+        <Cell name="Above 60" fill="#a44c9e" />
+      </Pie>
+      <Legend
+        iconType="circle"
+        layout="vertical"
+        verticalAlign="middle"
+        align="right"
+      />
+    </PieChart>
   )
 }
 
 export default VaccinationByAge
+
