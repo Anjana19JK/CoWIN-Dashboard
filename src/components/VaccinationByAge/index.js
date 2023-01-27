09116a12db9b8637eb7614c0a2811fe048ffc189
Anjana19JK <anjanajoseph199@gmail.com>
2023-01-27 23:03:53 +0530

    Update index.js

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,8 +1,8 @@
 import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 const VaccinationByAge = props => {
-  const {dataDetails} = props
-  const {count, age} = dataDetails
+  const {dataDetails2} = props
+  const {count, age} = dataDetails2
 
   return (
     <PieChart width={1000} height={300}>
